66d08dfa1eb486137137c7149fd7ba24e579ea1c
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-09 21:50:52 +0100

    add conditional rendering for cart removing dom manipulation with refs

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import './css/head.css'
 
 import logo from './images/logo.svg'
@@ -6,69 +6,75 @@ import cart from './images/icon-cart.svg'
 import user from './images/image-avatar.png'
 import product1thumb from './images/image-product-1-thumbnail.jpg'
 
-const Head = (props) => {
-    const box = useRef(null)
-    const box2 = useRef(null)
+const Cart = props => {
+    const [isEmpty, setIsEmpty] = useState(true)
 
-    function displayCart() {
-        box2.current.classList.toggle('display')
-
-        if(props.itemAmount > 0) {
-            box2.current.classList.remove('display')
-            box.current.classList.toggle('display')
-        }
-    }
-
-    return(
-        <>
-        <header>
-            <nav>
-                <div className='line'>
-                    <div className='nav-div-left'>
-                        <img src={logo} alt="Company Logo" className='logo' />
-                        <ul>
-                            <li className='links'><a href='#' className='links-a'>Collections</a></li>
-                            <li className='links'><a href='#' className='links-a'>Men</a></li>
-                            <li className='links'><a href='#' className='links-a'>Women</a></li>
-                            <li className='links'><a href='#' className='links-a'>About</a></li>
-                            <li className='links'><a href='#' className='links-a'>Contact</a></li>
-                        </ul>
-                    </div>
-                    <div className='nav-div-right'>
-                        <img src={cart} alt="cart"  onClick={displayCart} href="Cart" className='cart' />
-                        <img src={user} alt="user profile" className='user' />
-                    </div>
-
-                </div>
-            </nav>
-        </header>
-        <section>
-            <div className='display-cart-wrap'>
-                <div className='none' ref={box2}>
-                    <h4 className='men'>Cart</h4>
-                    <div className='cart-items-section'>
-                        <div className='cart-item-description'>
-                            <h4 className='hidden'>Your Cart Is Empty</h4>
-                        </div>
-                    </div>
-                </div>
-                <div className='none' ref={box}>
-                    <h4 className='oboi'>Cart</h4>
-                    <div className='cart-items-section'>
-                        <div className='cart-item-image-wrap'>
-                            <img alt='product thumbnail' src={product1thumb} />
-                        </div>
-                        <div className='cart-item-description'>
-                            <p>Fall Limited Edition Sneakers</p>
-                            <p>$125.00 x {props.itemAmount} = <strong>${props.finalPrice}</strong></p>
-                        </div>
-                    </div>
-                    <div className='checkout-div'>
-                        <h5 className='checkout light'>Checkout</h5>
-                    </div>
+    if(isEmpty == true && props.itemAmount > 0) setIsEmpty(false);
+    else if(isEmpty == false && props.itemAmount == 0) setIsEmpty(true);
+    
+    return (
+        (isEmpty) ?
+        <div className='none display' >
+            <h4 className='men'>Cart</h4>
+            <div className='cart-items-section'>
+                <div className='cart-item-description'>
+                    <h4 className='hidden'>Your Cart Is Empty</h4>
                 </div>
             </div>
-        </section>
+        </div>  :
+        <div className='none display' >
+            <h4 className='oboi'>Cart</h4>
+            <div className='cart-items-section'>
+                <div className='cart-item-image-wrap'>
+                    <img alt='product thumbnail' src={product1thumb} />
+                </div>
+                <div className='cart-item-description'>
+                    <p>Fall Limited Edition Sneakers</p>
+                    <p>$125.00 x {props.itemAmount} = <strong>${props.finalPrice}</strong></p>
+                </div>
+            </div>
+            <div className='checkout-div'>
+                <h5 className='checkout light'>Checkout</h5>
+            </div>
+        </div>
+    )
+}
+
+const Head = ({itemAmount, finalPrice}) => {
+    const [display, setDisplay] = useState(false);
+
+    function renderCart() {
+        setDisplay(prevState => !prevState)
+    }
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <div className='line'>
+                        <div className='nav-div-left'>
+                            <img src={logo} alt="Company Logo" className='logo' />
+                            <ul>
+                                <li className='links'><a href='#' className='links-a'>Collections</a></li>
+                                <li className='links'><a href='#' className='links-a'>Men</a></li>
+                                <li className='links'><a href='#' className='links-a'>Women</a></li>
+                                <li className='links'><a href='#' className='links-a'>About</a></li>
+                                <li className='links'><a href='#' className='links-a'>Contact</a></li>
+                            </ul>
+                        </div>
+                        <div className='nav-div-right'>
+                            <img src={cart} alt="cart" onClick={renderCart} href="Cart" className='cart' />
+                            <img src={user} alt="user profile" className='user' />
+                        </div>
+
+                    </div>
+                </nav>
+            </header>
+            <section>
+                <div className='display-cart-wrap'>
+                    {!display ? null : <Cart display={display} itemAmount={itemAmount} finalPrice={finalPrice} />}
+                </div>
+            </section>
         </>
     )
 }
